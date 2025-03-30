876d62c364e9b47260f54f09cc896f99abb7a3af
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-03-30 11:22:58 +0200

    IsNumberBitween function work with doubles

--- Validator.cpp
+++ Validator.cpp
@@ -5,8 +5,17 @@ using namespace std;
 
 int main()
 {
-    cout << Validator::IsNumberBitween(1, 2, 4);
-    cout << Validator::IsNumberBitween(2, 1, 4);
+    cout << "\n"
+         << Validator::IsNumberBitween(1, 2, 4);
+    cout << "\n"
+         << Validator::IsNumberBitween(2, 1, 4);
+
+    cout << "\n"
+         << Validator::IsNumberBitween(2.2, 1.1, 4.5);
+    cout << "\n"
+         << Validator::IsNumberBitween(2.4, 1.9, 4.8);
+
+         
 
     return 0;
 }
