0d550abdc04bbb5051dc369bbea4a3650a68d92e
Chiheb abiza <142681693+chihebabiza@users.noreply.github.com>
2025-03-30 12:51:42 +0100

    add Licence

--- Validator.cpp
+++ Validator.cpp
@@ -1,21 +1,42 @@
+// MIT License
+// Copyright (c) 2025 Abiza Chiheb
+// See the LICENSE file for full details.
 #include <iostream>
 #include "Validator.h"
 
-using namespace std;
-
 int main()
+
 {
-    cout << "\n"
-         << Validator::IsNumberBitween(1, 2, 4);
-    cout << "\n"
-         << Validator::IsNumberBitween(2, 1, 4);
+    cout << clsInputValidate::IsNumberBetween(5, 1, 10) << endl;
+    cout << clsInputValidate::IsNumberBetween(5.5, 1.3, 10.8) << endl;
 
-    cout << "\n"
-         << Validator::IsNumberBitween(2.2, 1.1, 4.5);
-    cout << "\n"
-         << Validator::IsNumberBitween(2.4, 1.9, 4.8);
+    cout << clsInputValidate::IsDateBetween(clsDate(),
+                                       clsDate(8,12,2022), 
+                                       clsDate(31, 12, 2022)) << endl;
+    
+    cout << clsInputValidate::IsDateBetween(clsDate(),
+                                       clsDate(31, 12, 2022),
+                                       clsDate(8, 12, 2022)) << endl;
+    
+    cout << "\nPlease Enter a Number:\n";
+    int x= clsInputValidate::ReadIntNumber("Invalid Number, Enter again:\n");
+    cout << "x=" << x;
 
-         
+    cout << "\nPlease Enter a Number between 1 and 5:\n";
+    int y = clsInputValidate::ReadIntNumberBetween(1,5,"Number is not within range, enter again:\n");
+    cout << "y=" << y;
+
+    cout << "\nPlease Enter a Double Number:\n";
+    double a = clsInputValidate::ReadDblNumber("Invalid Number, Enter again:\n");
+    cout << "a=" << a;
+
+    cout << "\nPlease Enter a Double Number between 1 and 5:\n";
+    double b = clsInputValidate::ReadDblNumberBetween(1, 5, "Number is not within range, enter again:\n");
+    cout << "b=" << b;
+
+    cout << endl << clsInputValidate::IsValideDate(clsDate(35,12,2022)) << endl;
+
+    system("pause");
 
     return 0;
 }
--- Validator.h
+++ Validator.h
@@ -1,14 +1,118 @@
+// MIT License
+// Copyright (c) 2025 Abiza Chiheb
+// See the LICENSE file for full details.
 #pragma once
-
 #include <iostream>
+#include <string>
+#include "../String-library/String.h"
+#include "../Date-library/Date.h"
+#include <limits>
 
-using namespace std;
-
-class Validator
+class clsInputValidate
 {
+
 public:
-    static bool IsNumberBitween(int num, int num1, int num2)
+    static bool IsNumberBetween(short Number, short From, short To)
     {
-        return (num > num1 && num < num2);
+        if (Number >= From && Number <= To)
+            return true;
+        else
+            return false;
+    }
+
+    static bool IsNumberBetween(int Number, int From, int To)
+    {
+        if (Number >= From && Number <= To)
+            return true;
+        else
+            return false;
+    }
+
+    static bool IsNumberBetween(float Number, float From, float To)
+    {
+        if (Number >= From && Number <= To)
+            return true;
+        else
+            return false;
+    }
+
+    static bool IsNumberBetween(double Number, double From, double To)
+    {
+        if (Number >= From && Number <= To)
+            return true;
+        else
+            return false;
+    }
+
+    static bool IsDateBetween(clsDate Date, clsDate From, clsDate To)
+    {
+        // Date>=From && Date<=To
+        if ((clsDate::IsDate1AfterDate2(Date, From) || clsDate::IsDate1EqualDate2(Date, From)) &&
+            (clsDate::IsDate1BeforeDate2(Date, To) || clsDate::IsDate1EqualDate2(Date, To)))
+        {
+            return true;
+        }
+
+        // Date>=To && Date<=From
+        if ((clsDate::IsDate1AfterDate2(Date, To) || clsDate::IsDate1EqualDate2(Date, To)) &&
+            (clsDate::IsDate1BeforeDate2(Date, From) || clsDate::IsDate1EqualDate2(Date, From)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    static int ReadIntNumber(string ErrorMessage = "Invalid Number, Enter again\n")
+    {
+        int Number;
+        while (!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << ErrorMessage;
+        }
+        return Number;
+    }
+
+    static int ReadIntNumberBetween(int From, int To, string ErrorMessage = "Number is not within range, Enter again:\n")
+    {
+        int Number = ReadIntNumber();
+
+        while (!IsNumberBetween(Number, From, To))
+        {
+            cout << ErrorMessage;
+            Number = ReadIntNumber();
+        }
+        return Number;
+    }
+
+    static double ReadDblNumber(string ErrorMessage = "Invalid Number, Enter again\n")
+    {
+        double Number;
+        while (!(cin >> Number))
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << ErrorMessage;
+        }
+        return Number;
+    }
+
+    static double ReadDblNumberBetween(double From, double To, string ErrorMessage = "Number is not within range, Enter again:\n")
+    {
+        double Number = ReadDblNumber();
+
+        while (!IsNumberBetween(Number, From, To))
+        {
+            cout << ErrorMessage;
+            Number = ReadDblNumber();
+        }
+        return Number;
+    }
+
+    static bool IsValideDate(clsDate Date)
+    {
+        return clsDate::IsValidDate(Date);
     }
 };
